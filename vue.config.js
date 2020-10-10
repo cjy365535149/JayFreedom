d5bc861f213bbd791c111198c2f8b599ada37ea3
丛景宇(jycong) <jycong@ap.whchem.com>
2020-10-10 15:25:35 +0800

    与nginx代理完成了基本跳转的配置，并且做了一些优化配置。

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,19 @@
+const path = require('path');
+const webpack = require('webpack')
+
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const productionGzipExtensions = ['js', 'css']
+const isProduction = process.env.NODE_ENV === 'production'
+
 module.exports = {
   lintOnSave: false,
-
-  //jay add start
-  //jay add end
   devServer: {
-  port: process.env.NODE_ENV === 'production' ? 8080 : 8081,
-  proxy: {  //配置跨域
+  port: 8080,
+  proxy:
+  /* process.env.NODE_ENV !== 'production' ? */
+   {  //配置跨域
     '/api': {
-      target:
-       process.env.NODE_ENV === 'production' ? 'http://localhost:8080/jayequality' : 'http://localhost:8080',  //这里后台的地址模拟的;应该填写你们真实的后台接口
+      target: 'http://localhost:8081',  //这里后台的地址模拟的;应该填写你们真实的后台接口
       changOrigin: true,  //允许跨域
       pathRewrite: {
         /* 重写路径
@@ -16,8 +21,31 @@ module.exports = {
         '^/api': '' 
       }
     },
-  }
+  } 
 },
 
-  publicPath: '/jayfreedom/'
+  publicPath: '/jayfreedom/',
+  configureWebpack:{
+    resolve:{
+      alias:{
+        '@':path.resolve(__dirname, './src'),
+        '@i':path.resolve(__dirname, './src/assets'),
+      } 
+    },
+    plugins: [
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+      
+      // 下面是下载的插件的配置
+      new CompressionWebpackPlugin({
+        algorithm: 'gzip',
+        test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+        threshold: 10240,
+        minRatio: 0.8
+      }),
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 5, 
+        minChunkSize: 100
+      })
+    ]
+  }
 }
